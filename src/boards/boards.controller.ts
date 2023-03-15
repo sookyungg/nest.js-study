34b40253866381143fd388daedd272b5db1f1a7f
Sookyung Kang <thoosoo@naver.com>
2023-03-16 01:19:46 +0900

    test

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Board } from './boards.model';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+    
+
+   constructor(private boardsService: BoardsService){ }
+
+    @Get('/')   
+    getAllBoard(): Board[]{
+
+    return this.boardsService.getAllBoards();
+
+    }
+
+    @Post()
+    createBoard(
+        @Body('title') title: string,
+        @Body('description') description: string 
+    ): Board {
+        return this.boardsService.createBoard(title, description)
+    }
+}
